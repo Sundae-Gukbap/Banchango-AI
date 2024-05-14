bae91cb949ef892d0410365bcc52d783768ca14e
vrexpert <vrexpert2302@gmail.com>
2024-05-14 15:53:00 +0900

    chore: ktlint check

--- Android/app/src/main/java/com/sundaegukbap/banchango/ui/theme/Theme.kt
+++ Android/app/src/main/java/com/sundaegukbap/banchango/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.sundaegukbap.banchango.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
