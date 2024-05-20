fbf92e5f8f51b3f06e89adf718bba25bba40663b
Due_it <48638700+FhRh@users.noreply.github.com>
2024-05-20 15:42:42 +0900

    [Server] feat : 기본 환경세팅(#1) (#11)
    기본 환경세팅

---
+++ Server/banchango/src/main/java/com/sundaegukbap/banchango/BanchangoApplication.java
@@ -0,0 +1,13 @@
+package com.sundaegukbap.banchango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanchangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanchangoApplication.class, args);
+	}
+
+}
