5787085cb484ca66ad63d0745f63b369dbe1bce8
Choi SeongHoon <108349655+SeongHoonC@users.noreply.github.com>
2024-05-24 16:44:39 +0900

    [Android] feat: Android initial setting(#3) (#4)
    * Feat(banchango): android initial setting
    
    * Feat(banchango): add ktlint check
    
    * chore: delete workflow
    
    * chore: ktlint check
    
    * chore: ktlint check
    
    * chore: ktlint check

---
+++ Android/build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.1.0" apply false
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+}
