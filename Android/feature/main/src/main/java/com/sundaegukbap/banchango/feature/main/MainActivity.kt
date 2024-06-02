8dcfac6a744fe9739ec2c7dd852784f652bb76c3
vrexpert <vrexpert2302@gmail.com>
2024-06-02 14:52:17 +0900

    feat: Main Activity 에서 Navigation 정의
    (cherry picked from commit 94a90f6ac472058d2f2e284bce79c9b137f99b8b)

--- Android/feature/main/src/main/java/com/sundaegukbap/banchango/feature/main/MainActivity.kt
+++ Android/feature/main/src/main/java/com/sundaegukbap/banchango/feature/main/MainActivity.kt
@@ -1,32 +1,35 @@
-package com.sundaegukbap.banchango.presentation.reciperecommend
+package com.sundaegukbap.banchango.feature.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.sundaegukbap.banchango.ui.theme.BanchangoTheme
+import com.sundaegukbap.banchango.core.designsystem.theme.BanchangoTheme
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: RecipeRecommendViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        viewModel.getRecipeRecommendation()
         setContent {
             BanchangoTheme {
+                val navController = rememberMainNavigator()
+
                 val systemUiController = rememberSystemUiController()
                 systemUiController.setSystemBarsColor(color = Color(0xBFFFFFFF), darkIcons = true)
                 systemUiController.setNavigationBarColor(
                     color = Color(0xFFFFFFFF)
                 )
-                RecipesRecommendScreen()
+
+                MainScreen(
+                    navigator = navController,
+                    onChangeDarkTheme = {}
+                )
             }
         }
     }
---
+++ Android/feature/main/src/main/java/com/sundaegukbap/banchango/feature/main/MainNavigator.kt
@@ -0,0 +1,101 @@
+package com.sundaegukbap.banchango.feature.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import com.sundaegukbap.banchango.feature.home.navigation.navigateHome
+import com.sundaegukbap.banchango.feature.reciperecommend.navigation.navigateRecipeRecommend
+import com.sundaegukbap.banchango.navigation.MainTabRoute
+import com.sundaegukbap.banchango.navigation.Route
+
+internal class MainNavigator(
+    val navController: NavHostController,
+) {
+    private val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val startDestination = MainTab.RECIPE_RECOMMEND.route
+
+    val currentTab: MainTab?
+        @Composable get() = MainTab.find { tab ->
+            currentDestination?.hasRoute(tab::class) == true
+        }
+
+    fun navigate(tab: MainTab) {
+        val navOptions = navOptions {
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+            }
+            launchSingleTop = true
+            restoreState = true
+        }
+
+        when (tab) {
+            MainTab.RECIPE_RECOMMEND -> navController.navigateRecipeRecommend(navOptions)
+            MainTab.HOME -> navController.navigateHome(navOptions)
+        }
+    }
+
+    private fun popBackStack() {
+        navController.popBackStack()
+    }
+
+    fun popBackStackIfNotHome() {
+        if (!isSameCurrentDestination<MainTabRoute.Home>()) {
+            popBackStack()
+        }
+    }
+
+    private inline fun <reified T : Route> isSameCurrentDestination(): Boolean {
+        return navController.currentDestination?.hasRoute<T>() == true
+    }
+
+    @Composable
+    fun shouldShowBottomBar() = MainTab.contains {
+        currentDestination?.hasRoute(it::class) == true
+    }
+}
+
+@Composable
+internal fun rememberMainNavigator(
+    navController: NavHostController = rememberNavController(),
+): MainNavigator = remember(navController) {
+    MainNavigator(navController)
+}
+
+
+internal enum class MainTab(
+    val iconResId: Int,
+    internal val contentDescription: String,
+    val route: MainTabRoute,
+) {
+    HOME(
+        iconResId = R.drawable.ic_home,
+        contentDescription = "홈",
+        MainTabRoute.Home
+    ),
+    RECIPE_RECOMMEND(
+        iconResId = R.drawable.ic_recipe_recommend,
+        contentDescription = "레시피 추천",
+        MainTabRoute.RecipeRecommend,
+    );
+
+    companion object {
+        @Composable
+        fun find(predicate: @Composable (MainTabRoute) -> Boolean): MainTab? {
+            return entries.find { predicate(it.route) }
+        }
+
+        @Composable
+        fun contains(predicate: @Composable (Route) -> Boolean): Boolean {
+            return entries.map { it.route }.any { predicate(it) }
+        }
+    }
+}
