5a119d8ab41b688219e10ee2f738d5498db062b4
vrexpert <vrexpert2302@gmail.com>
2024-06-02 14:53:48 +0900

    feat(navigation): 홈, 레시피 추천, 레시피 상세로 이동할 수 있다
    (cherry picked from commit b61ffdf6c5b1fb20ebec4495fc874f653ff3b392)

---
+++ Android/core/navigation/src/main/java/com/sundaegukbap/banchango/navigation/Route.kt
@@ -0,0 +1,16 @@
+package com.sundaegukbap.banchango.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed interface Route {
+    @Serializable
+    data class RecipeDetail(val recipeId: String) : Route
+}
+
+sealed interface MainTabRoute : Route {
+    @Serializable
+    data object RecipeRecommend : MainTabRoute
+
+    @Serializable
+    data object Home : MainTabRoute
+}
