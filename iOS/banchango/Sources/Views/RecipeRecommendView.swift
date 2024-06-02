7980b7461925f8dd6babecb65ae4c738f5c869cf
김민석|Minseok Kim <alstjr7437@naver.com>
2024-06-03 02:23:52 +0900

    Feat: 수직 스크롤 페이징 View 그리기

--- iOS/banchango/Sources/Views/RecipeRecommendView.swift
+++ iOS/banchango/Sources/Views/RecipeRecommendView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct RecipeRecommendView: View {
+    let colors:[Color] = [.purple, .pink, .orange]
+    let texts: [String] = ["순대국밥", "김치찌개", "Recipe 3"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+            GeometryReader { proxy in
+                ScrollView(.vertical) {
+                    VStack(spacing: 0) {
+                        ForEach(colors.indices, id: \.self) { index in
+                            ZStack {
+                                colors[index]
+                                Circle()
+                                    .fill(Color.white)
+                                    .frame(width: 200, height: 200)
+                                    .overlay(
+                                        Text(texts[index])
+                                            .font(.largeTitle)
+                                            .foregroundColor(.black)
+                                    )
+                            }
+                            .frame(width: proxy.size.width, height: proxy.size.height)
+                        }
+                    }
+                }
+            }.onAppear() {
+                UIScrollView.appearance().isPagingEnabled = true
+            }
+        }
 }
 
 #Preview {
