251251dca5a5634839bc803655a38ca9cb469029
vrexpert <vrexpert2302@gmail.com>
2024-06-04 15:19:31 +0900

    feat(designsystem): Color 추가

--- Android/core/designsystem/src/main/java/com/sundaegukbap/banchango/core/designsystem/theme/Color.kt
+++ Android/core/designsystem/src/main/java/com/sundaegukbap/banchango/core/designsystem/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Orange = Color(0xFFFF7A00)
+val LightBeige = Color(0xFFF5EB)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+// 투명도 50%
+val SemiTransparentOrange = Color(0x80FF7A00)
+val SemiTransparentBlack = Color(0x80000000)
