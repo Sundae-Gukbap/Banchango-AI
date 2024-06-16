d66b47d021d657e14dd07cee59bd4d0db3562481
김민석|Minseok Kim <alstjr7437@naver.com>
2024-06-16 15:34:50 +0900

    feat: 레시피 카드 뷰 파일 생성 및 이동

---
+++ iOS/banchango/Sources/Presentation/RecipeRecommend/View/RecipeCardView.swift
@@ -0,0 +1,52 @@
+//
+//  RecipeCardView.swift
+//  banchango
+//
+//  Created by KimMinSeok on 6/16/24.
+//
+
+import SwiftUI
+
+struct RecipeCardView: View {
+    let recipe: Recipe
+    func haveNeedIngr(have : [String], need : [String]) -> String {
+        let haveCount = have.count
+        let needCount = need.count
+        
+        return "\(haveCount)/\(needCount+haveCount)"
+    }
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.clear)
+                .frame(width: 300, height: 550)
+                .background(
+                    AsyncImage(url: URL(string: recipe.image)) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 300, height: 550)
+                    } placeholder: {
+                        Color.gray.opacity(0.3)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                )
+                .overlay(
+                    VStack {
+                        Text(recipe.name)
+                            .font(.custom("Inter-Bold", size: 30))
+                            .lineLimit(2)
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text(haveNeedIngr(have: recipe.have, need:recipe.need))
+                            .font(.custom("Inter-Bold", size: 30))
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                )
+        }.onTapGesture {
+            print("Hello")
+        }
+    }
+}
--- iOS/banchango/Sources/Presentation/RecipeRecommend/View/RecipeRecommendView.swift
+++ iOS/banchango/Sources/Presentation/RecipeRecommend/View/RecipeRecommendView.swift
@@ -15,8 +15,6 @@ struct RecipeRecommendView: View {
         recipeRecommendVM.getRecipeRecommend()
     }
     
-    let colors:[Color] = [.purple, .pink, .orange]
-    
     var body: some View {
         GeometryReader { proxy in
             ScrollView(.vertical) {
@@ -29,43 +27,6 @@ struct RecipeRecommendView: View {
             }
         }.onAppear() {
             UIScrollView.appearance().isPagingEnabled = true
-        }
-    }
-}
-
-
-struct RecipeCardView: View {
-    let recipe: Recipe
-    
-    var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.clear)
-                .background(
-                    AsyncImage(url: URL(string: recipe.image)) { image in
-                        image
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 300, height: 550)
-                    } placeholder: {
-                        Color.gray.opacity(0.3)
-                    }
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                )
-                .frame(width: 300, height: 550)
-                .overlay(
-                    VStack {
-                        Text(recipe.name)
-                            .font(.largeTitle)
-                            .foregroundColor(.black)
-                        Spacer()
-                        Text(recipe.introduction)
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                            .multilineTextAlignment(.center)
-                    }
-                    .padding()
-                )
         }
     }
 }
