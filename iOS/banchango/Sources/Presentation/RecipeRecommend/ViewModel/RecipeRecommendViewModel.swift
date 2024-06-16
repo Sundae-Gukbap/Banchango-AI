940aea6f4fa96622c4e4cb3679f666fdaa63157e
김민석|Minseok Kim <alstjr7437@naver.com>
2024-06-16 15:35:41 +0900

    feat: 더미 데이터 레시피 load 테스트 및 자료 변경

--- iOS/banchango/Sources/Presentation/RecipeRecommend/ViewModel/RecipeRecommendViewModel.swift
+++ iOS/banchango/Sources/Presentation/RecipeRecommend/ViewModel/RecipeRecommendViewModel.swift
@@ -25,6 +25,7 @@ class RecipeRecommendViewModel: ObservableObject {
             .sink(receiveCompletion: { completion in
                 switch completion {
                 case .failure(let error):
+                    self.loadDummyData() // 네트워크 요청 실패 시 더미 데이터 로드
                     print("네트워크 오류: \(error)")
                 case .finished:
                     print("추천 레시피 데이터 요청이 완료되었습니다.")
@@ -34,4 +35,20 @@ class RecipeRecommendViewModel: ObservableObject {
             })
             .store(in: &cancellables)
     }
+    
+    private func loadDummyData() {
+        if let url = Bundle.main.url(forResource: "Recipe", withExtension: "json"),
+           let data = try? Data(contentsOf: url) {
+            let decoder = JSONDecoder()
+            if let dummyRecipes = try? decoder.decode([Recipe].self, from: data) {
+                self.recipes = dummyRecipes
+                print("더미 데이터를 로드했습니다.")
+            } else {
+                print("더미 데이터를 디코딩하는 데 실패했습니다.")
+            }
+        } else {
+            print("더미 데이터 파일을 찾을 수 없습니다.")
+        }
+    }
+    
 }
