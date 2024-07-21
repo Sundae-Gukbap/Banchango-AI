84c918252c7b97ca444bfee85df79e43644be8dc
SeongHoonC <vrexpert2302@gmail.com>
2024-07-21 17:30:59 +0900

    feat(RecipeDetailScreen): Recipe 이미지에 그림자 그라데이션을 넣는다.

--- Android/feature/recipe/src/main/java/com/sundaegukbap/banchango/feature/recipe/detail/RecipeDetailScreen.kt
+++ Android/feature/recipe/src/main/java/com/sundaegukbap/banchango/feature/recipe/detail/RecipeDetailScreen.kt
@@ -1,50 +1,41 @@
 package com.sundaegukbap.banchango.feature.recipe.detail
 
-import android.content.Intent
-import android.net.Uri
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.SheetValue
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat.startActivity
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sundaegukbap.banchango.LikableRecipe
 import com.sundaegukbap.banchango.Recipe
 import com.sundaegukbap.banchango.RecipeDifficulty
 import com.sundaegukbap.banchango.core.designsystem.component.NetworkImage
-import com.sundaegukbap.banchango.core.designsystem.theme.LightOrange
-import com.sundaegukbap.banchango.core.designsystem.theme.Orange
 
 @Composable
 fun RecipeDetailRoute(
@@ -106,7 +97,6 @@ private fun RecipeDetailLoading() {
         CircularProgressIndicator()
     }
 }
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -179,125 +169,37 @@ private fun RecipeDetailScreen(
             Modifier
                 .align(Alignment.BottomCenter)
                 .padding(bottom = 60.dp),
-            colors = ButtonColors(
-                containerColor = Orange,
-                contentColor = Color.White,
-                disabledContainerColor = LightOrange,
-                disabledContentColor = Color.White
-            ),
-            onClick = {
-                context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(recipe.link)))
-            },
-        ) {
-            Text(text = "레시피 이동하기")
-        }
+        )
     }
 }
 
 @Composable
-private fun RecipeDetailCard(
-    recipe: Recipe,
-    modifier: Modifier = Modifier
+private fun RecipeImage(
+    imageUrl: String,
+    modifier: Modifier,
 ) {
-    Box(
-        modifier = modifier
-            .fillMaxSize()
-    ) {
-
-        Column(
-            Modifier.padding(horizontal = 16.dp)
-        ) {
-            RecipeDetailInfo(
-                difficulty = recipe.difficulty,
-                serving = recipe.servings,
-                cookingTime = recipe.cookingTime,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.CenterHorizontally),
-                paddingHorizontal = 32,
-            )
-
-            Text(
-                modifier = Modifier.padding(top = 20.dp),
-                text = recipe.name,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold
-            )
-
-            Text(
-                text = recipe.introduction,
-                fontSize = 12.sp,
-                modifier = Modifier.padding(top = 12.dp)
-            )
-
-
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 20.dp)
-            ) {
-                Text(
-                    text = "식재료",
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.Bold,
-                )
-                Text(
-                    text = "${recipe.have.size}/${recipe.have.size + recipe.need.size}",
-                    color = Color.White,
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier
-                        .background(color = Orange, shape = CircleShape)
-                        .padding(horizontal = 8.dp)
-                        .align(Alignment.CenterEnd)
-                )
-            }
-
-            HorizontalDivider(modifier = Modifier.padding(top = 12.dp), color = Orange)
-            LazyRow(
-                modifier = Modifier.padding(top = 16.dp),
-                horizontalArrangement = Arrangement.spacedBy(12.dp),
-            ) {
-                items(recipe.have) {
-                    Box(
-                        modifier = Modifier
-                            .border(1.dp, color = Orange, shape = CircleShape)
-                            .background(color = Color.White, shape = CircleShape)
-                    ) {
-                        Text(modifier = Modifier.padding(16.dp), text = it)
-                    }
-                }
-                items(recipe.need) {
-                    Box(
-                        modifier = Modifier
-                            .border(1.dp, color = Orange, shape = CircleShape)
-                            .background(color = LightOrange, shape = CircleShape)
-                    ) {
-                        Text(modifier = Modifier.padding(16.dp), text = it)
-                    }
-                }
-            }
-        }
+    Box(modifier = modifier) {
+        NetworkImage(
+            url = imageUrl,
+            modifier = Modifier.fillMaxSize(),
+        )
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(
+                    brush =
+                    Brush.verticalGradient(
+                        colors =
+                        listOf(
+                            Color.Black.copy(alpha = 0.3f),
+                            Color.Transparent,
+                        ),
+                        startY = 0f,
+                        endY = Float.POSITIVE_INFINITY,
+                    ),
+                ),
+        )
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun RecipeDetailCardPreview() {
-    RecipeDetailCard(
-        recipe = Recipe(
-            id = 1,
-            name = "간장계란볶음밥",
-            introduction = "아주 간단하면서 맛있는 계란간장볶음밥으로 한끼식사 만들어보세요. 아이들이 더 좋아할거예요.",
-            image = "https://recipe1.ezmember.co.kr/cache/recipe/2018/05/26/d0c6701bc673ac5c18183b47212a58571.jpg",
-            link = "https://www.10000recipe.com/recipe/6889616",
-            cookingTime = 10,
-            servings = 2,
-            difficulty = RecipeDifficulty.BEGINNER,
-            have = listOf("계란", "간장"),
-            need = listOf("식초", "당근", "감자"),
-        ),
-    )
 }
 
 @Preview(showBackground = true)
