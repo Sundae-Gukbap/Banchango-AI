b55ef89a59ab0308afc6d5008b9724f3ef66bc1e
FhRh <yiyop@naver.com>
2024-09-22 17:52:00 +0900

    feat : 창고 전체 목록 조회 기능 추가

--- Server/banchango/src/main/java/com/sundaegukbap/banchango/container/dto/dto/ContainerDto.java
+++ Server/banchango/src/main/java/com/sundaegukbap/banchango/container/dto/dto/ContainerDto.java
@@ -2,6 +2,9 @@ package com.sundaegukbap.banchango.container.dto.dto;
 
 import com.sundaegukbap.banchango.container.domain.Container;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public record ContainerDto(
         Long containerId,
         String containerName
@@ -10,5 +13,11 @@ public record ContainerDto(
         return new ContainerDto(
                 container.getId(),
                 container.getName());
+    }
+
+    public static List<ContainerDto> of(List<Container> containers){
+        return containers.stream()
+                .map(ContainerDto::of)
+                .collect(Collectors.toList());
     }
 }
--- Server/banchango/src/main/java/com/sundaegukbap/banchango/container/presentation/ContainerController.java
+++ Server/banchango/src/main/java/com/sundaegukbap/banchango/container/presentation/ContainerController.java
@@ -2,6 +2,7 @@ package com.sundaegukbap.banchango.container.presentation;
 
 import com.sundaegukbap.banchango.container.application.ContainerService;
 import com.sundaegukbap.banchango.container.dto.ContainerInsertRequest;
+import com.sundaegukbap.banchango.container.dto.dto.ContainerDto;
 import com.sundaegukbap.banchango.ingredient.dto.IngredientInsertRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -9,9 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/container")
-@Tag(name = "재료 관련 컨트롤러")
+@Tag(name = "식자재 창고 관련 컨트롤러")
 public class ContainerController {
     private final ContainerService containerService;
 
@@ -25,5 +28,12 @@ public class ContainerController {
                                                    @RequestBody ContainerInsertRequest request) {
         containerService.createContainer(userId, request);
         return new ResponseEntity<>("success add container", HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    @Operation(summary = "전체 창고 목록 조회", description = "전체 창고 목록을 조회합니다.")
+    public ResponseEntity<List<ContainerDto>> getContainers(@PathVariable("userId") Long userId) {
+        List<ContainerDto> result = containerService.getAllContainers(userId);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
