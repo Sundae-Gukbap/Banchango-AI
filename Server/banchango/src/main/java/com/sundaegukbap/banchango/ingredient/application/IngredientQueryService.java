7b88bd6c49dd75aefe90685312e12c9c31085283
FhRh <yiyop@naver.com>
2024-10-01 15:33:10 +0900

    refact : 사용하지 않는 메서드 제거

--- Server/banchango/src/main/java/com/sundaegukbap/banchango/ingredient/application/IngredientQueryService.java
+++ Server/banchango/src/main/java/com/sundaegukbap/banchango/ingredient/application/IngredientQueryService.java
@@ -1,18 +1,15 @@
 package com.sundaegukbap.banchango.ingredient.application;
+
 import com.sundaegukbap.banchango.container.domain.Container;
 import com.sundaegukbap.banchango.container.repository.ContainerRepository;
 import com.sundaegukbap.banchango.ingredient.domain.ContainerIngredient;
-import com.sundaegukbap.banchango.ingredient.dto.CategoryIngredientResponse;
-import com.sundaegukbap.banchango.ingredient.dto.CategoryIngredientResponses;
-import com.sundaegukbap.banchango.ingredient.dto.IngredientDetailResponse;
-import com.sundaegukbap.banchango.ingredient.dto.IngredientDetailResponses;
 import com.sundaegukbap.banchango.ingredient.dto.dto.ContainerIngredientDto;
 import com.sundaegukbap.banchango.ingredient.dto.dto.ContainerIngredientDtos;
 import com.sundaegukbap.banchango.ingredient.repository.ContainerIngredientRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class IngredientQueryService {
@@ -42,26 +39,4 @@ public class IngredientQueryService {
                 .orElseThrow(() -> new NoSuchElementException("no ingredient in container"));
         return ContainerIngredientDto.of(containerIngredient);
     }
-
-    public CategoryIngredientResponses getCategoryIngredientResponses(Long containerId) {
-        List<ContainerIngredient> containerIngredients = containerIngredientRepository.findAllByContainerId(containerId);
-
-        Map<String, List<ContainerIngredient>> kindIngredientsMap = containerIngredients.stream()
-                .collect(Collectors.groupingBy(userHavingIngredient -> userHavingIngredient.getIngredient().getKind()));
-
-        List<CategoryIngredientResponse> categoryIngredientResponseList = new ArrayList<>();
-        kindIngredientsMap.forEach((kind, userHavingIngredientsList) -> {
-            List<IngredientDetailResponse> ingredientDetailResponseList = userHavingIngredientsList.stream()
-                    .map(IngredientDetailResponse::of)
-                    .collect(Collectors.toList());
-
-            IngredientDetailResponses ingredientDetailResponses = IngredientDetailResponses.of(ingredientDetailResponseList);
-
-            categoryIngredientResponseList.add(CategoryIngredientResponse.of(kind, ingredientDetailResponses));
-        });
-
-        return CategoryIngredientResponses.of(categoryIngredientResponseList);
-    }
-
-
 }
