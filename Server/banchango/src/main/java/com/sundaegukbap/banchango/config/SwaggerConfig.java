85a7a57436c7c2331e66ccf836e3b68b295fa665
FhRh <yiyop@naver.com>
2024-10-03 03:36:28 +0900

    feat : https 설정 추가

---
+++ Server/banchango/src/main/java/com/sundaegukbap/banchango/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.sundaegukbap.banchango.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .version("1.0")
+                .title("반찬고 AI")
+                .description("설명");
+
+        Server server = new Server();
+        server.setUrl("https://backendu.com");
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(server));
+    }
+
+}
